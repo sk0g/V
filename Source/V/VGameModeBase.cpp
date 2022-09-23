cecb1755cc89fd40bda9c283d594f3ca447e4d95
sk0g <ani.mehta@mail.com>
2022-09-24 04:55:59 +1000

    Init UE files

---
+++ Source/V/VGameModeBase.cpp
@@ -0,0 +1,2 @@
+#include "VGameModeBase.h"
+
---
+++ Source/V/VGameModeBase.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VGameModeBase.generated.h"
+
+
+UCLASS()
+class V_API AVGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
